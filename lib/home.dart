50df65af3ad00d4aa4da8d54da4c1aa73a3946ab
Jitesh117 <sahookumarjitesh@gmail.com>
2022-07-31 15:06:29 +0530

    initial commit

---
+++ lib/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300,
+      
+    );
+  }
+}
